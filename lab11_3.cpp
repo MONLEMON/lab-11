13d77ec4d67920acc3f58f3be9a8ad00c99b83ac
Surabordin <bluelimec@gmail.com>
2021-12-17 17:39:13 +0700

    lab 11-3 complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main (){
+	ifstream source;
+	source.open("score.txt");
+    float i = 0 ;
+    float sum = 0 ;
+    float sum2 = 0;
+    float M,N;
+    string n;
+    while (getline(source,n)){
+        sum += atof(n.c_str()); ;
+        sum2 += pow(atof(n.c_str()),2);
+        i++;
+    }
+    cout << "Number of data = "<< i <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< (1/i)*(sum)<<"\n" ;
+    M = (1/i)*(sum) ;
+    N = sum2/i;
+    cout << "Standard deviation = " << sqrt(N-(M*M)) ;
+}    
